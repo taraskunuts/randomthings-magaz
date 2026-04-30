421bdcb55b88f382e6d993b08a6aca49677789bf
taraskukuts <taras.kunytskyi@gmail.com>
2026-04-30 21:40:00 +0300

    push

--- my-app/src/pages/Register.jsx
+++ my-app/src/pages/Register.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  function handleRegister() {
+    setError("");
+    if (!username || !password) {
+      setError("Заповни всі поля");
+      return;
+    }
+
+    if (password.length < 4) {
+      setError("Пароль має бути мінімум 4 символи");
+      return;
+    }
+    const user = {
+      username,
+      password,
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    alert("Реєстрація успішна!");
+    setUsername("");
+    setPassword("");
+  }
+
   return (
-    <header>
-      <h1>Мій сайт</h1>
-    </header>
+    <div style={{ maxWidth: "300px" }}>
+      <h2>Register</h2>
+
+      <input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <button onClick={handleRegister}>
+        Create account
+      </button>
+    </div>
   );
 }
