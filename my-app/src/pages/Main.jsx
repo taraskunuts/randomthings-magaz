7195e04b747b4d6b157ef9d567e54617f381397f
supriseeeeef <romankorniiets@gmail.com>
2026-04-30 21:36:19 +0300

    added products

--- my-app/src/pages/Main.jsx
+++ my-app/src/pages/Main.jsx
@@ -1,7 +1,18 @@
+import "./Main.css";
+import products from "./Products.json";
+
 export default function Main() {
   return (
-    <header>
-      <h1>Мій сайт</h1>
-    </header>
+    <>
+      <div className="grid">
+        {products.map(product => (
+          <div className="card" key={product.id}>
+            <img src={product.url} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>{product.price} грн</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
